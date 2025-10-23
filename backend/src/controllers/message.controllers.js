0b6b44f96c07b6e72e58fe1dbb98c556158a46b4
Zaid Malik <mzaidmalik25@gmail.com>
2025-10-23 16:21:38 +0530

    feat(chat): implement Message and Conversation schemas with create/send message functionality

---
+++ backend/src/controllers/message.controllers.js
@@ -0,0 +1,51 @@
+import Conversation from "../models/conversation.model.js"
+import Message from "../models/message.model.js"
+import { uploadImage } from "../service/imageKit.service.js"
+
+export const sendMessage = async (req, res) => {
+  try {
+    const sender = req.userId;
+    const {reciever} = req.params; 
+    const { message } = req.body;
+
+    if (!reciever || !message) {
+      return res.status(400).json({ message: "Message and receiver are required" });
+    }
+
+    // Handle image upload if present
+    let image;
+    if (req.file) {
+      const uploaded = await uploadImage(req.file);
+      image = uploaded?.url;
+    }
+
+    // Create new message
+    const newMessage = await Message.create({
+      sender,
+      reciever,
+      message,
+      image: image || undefined,
+    });
+
+    // Find existing conversation
+    let conversation = await Conversation.findOne({
+      participants: { $all: [sender, reciever] },
+    });
+
+    // If conversation doesn't exist, create new
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [sender, reciever],
+        messages: [newMessage._id],
+      });
+    } else {
+      // Push new message to existing conversation
+      conversation.messages.push(newMessage._id);
+      await conversation.save();
+    }
+
+    return res.status(201).json(newMessage);
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
